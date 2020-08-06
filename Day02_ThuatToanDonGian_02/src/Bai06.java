1ca02cf631f1106f1715de5a60a048e0a452afd3
PC <PC@PC-PC>
2020-08-06 10:33:25 +0700

    Bai 06

---
+++ Day02_ThuatToanDonGian_02/src/Bai06.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Bai06 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Mời nhập số tự nhiên n:   ");
+		int n = sc.nextInt();
+		int m = 0;
+		
+		for ( int i=1; m <= n-2; i++) {
+			for ( int j=1; j <= i; j++) {
+				if (m <= n-2) {
+					m += 2;
+					System.out.println( m );
+				}
+				else break;
+			}
+			if (m <= n-2) {
+				m += 2;
+				System.out.println( m * (-1));
+			} else break;
+		}
+	}
+}
